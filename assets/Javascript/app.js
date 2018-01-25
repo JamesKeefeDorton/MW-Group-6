7a1ea4cc9f1a354adab659b1a5a73c0ec9ec34d0
Erik Plimmer <Elp013@Gmail.com>
2018-01-24 21:07:19 -0500

    [updating to do list]

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -2,11 +2,8 @@
    //================================================
    // creating new york times search page using locations on google maps 
    // number of artices reuturned (10 by default)
-	// users clicks locations on map then search near by city (population of ???) and store in varible 
-   // create a new york times query for that choosen location 
-   // query can return number of nearest locations is (max 20 by default)
-   // can choose radius 
-   // each query can return 10 articles 
+	// users clicks locations on map then search the state where user clicked and store in varible 
+   // create a new york times query for that choosen location (sematic api) 
    // comment every line for easy understaning 
    //=============================================================================
    // Bouns features 
@@ -15,8 +12,7 @@
    // dates staring and ending 
    // topics 
    // sorting atricles by popularity
-   // 
-
+  
 
 
 
