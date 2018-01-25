ec4944e2444a67f835ae7660a15e8dc6d3719971
Erik Plimmer <Elp013@Gmail.com>
2018-01-24 20:33:05 -0500

    update1/24/2018

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,7 +1,36 @@
+   // to do list
+   //================================================
+   // creating new york times search page using locations on google maps 
+   // number of artices reuturned (10 by default)
+	// users clicks locations on map then store lat and long, in varible 
+   // create a new york times query for that choosen location 
+   // query can return number of nearest locations is (max 20 by default)
+   // can choose radius 
+   // each query can return 10 articles 
+   // comment every line for easy understaning 
+   //=============================================================================
+   // Bouns features 
+   // sorting articles by date 
+   // provide users location (signing in to google account)
+   // dates staring and ending 
+   // topics 
+   // sorting atricles by popularity
+   // 
+
+
+
+
+
+
+   // this displays map
     var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
+          center: {lat: 35.855007, lng: -78.840645},
           zoom: 8
+        });
+          var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
         });
       }
