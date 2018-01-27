40112c3dd852d69fbb59322f77f3b4a66a30736e
JamesKeefeDorton <jameskeefedorton@gmail.com>
2018-01-27 11:52:53 -0500

    before break

--- assets/Javascript/nyt.js
+++ assets/Javascript/nyt.js
@@ -1,18 +1,38 @@
 $(document).ready(function(){
+	var conceptName;
 	var state = "Washington";
-	var url = "https://cors-anywhere.herokuapp.com/https://api.nytimes.com/svc/semantic/v2/geocodes/query.json";
-	url += '?' + $.param({
+	var urlGeo = "https://cors-anywhere.herokuapp.com/https://api.nytimes.com/svc/semantic/v2/geocodes/query.json";
+	urlGeo += '?' + $.param({
 		'api-key': "b9f91d369ff59547cd47b931d8cbc56b:0:74623931",
 		'name': state,
 		'feature_class': "A"
 	});
 	$.ajax({
-		url: url,
+		url: urlGeo,
 		method: 'GET'
 	}).done(function(result) {
 		console.log(result);
-		var conceptName = result.results[0]["concept_name"];
+		conceptName = result.results[0]["concept_name"];
 		console.log(conceptName);
+		for (let i = 0; i < conceptName.length; i++) {
+			if (conceptName[i] === " ") {
+				conceptName = conceptName.slice(0, i) + "%20" + conceptName.slice(i+1);
+			}
+		}
+		var urlSem = "https://cors-anywhere.herokuapp.com/https://api.nytimes.com/svc/semantic/v2/concept/name/nytd_geo/" + conceptName;
+		urlSem += '?' + $.param({
+		'api-key': "b9f91d369ff59547cd47b931d8cbc56b:0:74623931",
+		'fields': "article_list"
+		});
+		console.log(urlSem);
+		$.ajax({
+			url: urlSem,
+			method: 'GET'
+		}).done(function(result) {
+			console.log(result);
+		}).fail(function(err) {
+			throw err;
+		});
 	}).fail(function(err) {
 		throw err;
 	});
