56e9976c1222ee5af0569d6f5c3c4b16916cc9b9
Erik Plimmer <Elp013@Gmail.com>
2018-01-31 18:43:35 -0500

    update search bar

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -8,11 +8,37 @@
 
         }); // map object end
 
+
+        var geocoder = new google.maps.Geocoder();
+
+
+        document.getElementById('submit').addEventListener('click', function() {
+          geocodeAddress(geocoder, map);
+        });
+      }
+
+      function geocodeAddress(geocoder, resultsMap) {
+        var address = document.getElementById('address').value;
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location
+            });
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
               map.addListener('click', function(e) {
                 placeMarkerAndPanTo(e.latLng, map);
 
               }); // click function end
             }
+
+
+
+
 
                 function setMapOnAll(map) {
         for (let i = 0; i < markers.length; i++) {
